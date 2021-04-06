7dcab5c1b74e50ca83d6af3fc6f8a7006eea68ee
tianliwu89 <54875510+tianliwu89@users.noreply.github.com>
2021-04-06 00:21:21 -0600

    Update setupKafka.sh
    Added disown

--- kafka/setupKafka.sh
+++ kafka/setupKafka.sh
@@ -29,10 +29,10 @@ KAFKA_DNS=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/ho
 #KAFKA_DNS=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 if [ "$PWD" = "/" ]; then
 	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" kafka_2.13-2.7.0/config/server.properties
-	sudo kafka_2.13-2.7.0/bin/zookeeper-server-start.sh kafka_2.13-2.7.0/config/zookeeper.properties &
-	sudo kafka_2.13-2.7.0/bin/kafka-server-start.sh kafka_2.13-2.7.0/config/server.properties &
+	sudo kafka_2.13-2.7.0/bin/zookeeper-server-start.sh kafka_2.13-2.7.0/config/zookeeper.properties & disown
+	sudo kafka_2.13-2.7.0/bin/kafka-server-start.sh kafka_2.13-2.7.0/config/server.properties & disown
 else
 	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" $(pwd)/kafka_2.13-2.7.0/config/server.properties
-	sudo $(pwd)/kafka_2.13-2.7.0/bin/zookeeper-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/zookeeper.properties &
-	sudo $(pwd)/kafka_2.13-2.7.0/bin/kafka-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/server.properties &
+	sudo $(pwd)/kafka_2.13-2.7.0/bin/zookeeper-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/zookeeper.properties & disown
+	sudo $(pwd)/kafka_2.13-2.7.0/bin/kafka-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/server.properties & disown
 fi
